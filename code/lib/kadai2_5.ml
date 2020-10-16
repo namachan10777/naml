e84c3d7f28d9a9aa7f1110103e71dbbdef148750
Nakano Masaki <namachan10777@gmail.com>
2020-10-16 17:06:51 +0900

    report3 without src

--- code/lib/kadai2_5.ml
+++ code/lib/kadai2_5.ml
@@ -17,6 +17,58 @@ type value_t =
     | IntVal of int
     | BoolVal of bool
 
+let rec eval2 =
+    function
+    | IntLit n -> IntVal n
+    | BoolLit b -> BoolVal b
+    | Plus (lhr, rhr) -> begin match (eval2 lhr, eval2 rhr) with
+        | (IntVal lhr, IntVal rhr) -> IntVal(lhr + rhr)
+        | _ -> failwith "integer value expected"
+    end
+    | Sub (lhr, rhr) -> begin match (eval2 lhr, eval2 rhr) with
+        | (IntVal lhr, IntVal rhr) -> IntVal(lhr + rhr)
+        | _ -> failwith "integer value expected"
+    end
+    | Times (lhr, rhr) -> begin match (eval2 lhr, eval2 rhr) with
+        | (IntVal lhr, IntVal rhr) -> IntVal(lhr * rhr)
+        | _ -> failwith "integer value expected"
+    end
+    | Div (lhr, rhr) -> begin match (eval2 lhr, eval2 rhr) with
+        | (IntVal _, IntVal 0) -> failwith "divided by 0"
+        | (IntVal lhr, IntVal rhr) -> IntVal(lhr / rhr)
+        | _ -> failwith "integer value expected"
+    end
+    | Greater (lhr, rhr) -> begin match (eval2 lhr, eval2 rhr) with
+        | (IntVal lhr, IntVal rhr) -> BoolVal(lhr > rhr)
+        | _ -> failwith "integer value expected"
+    end
+    | Less (lhr, rhr) -> begin match (eval2 lhr, eval2 rhr) with
+        | (IntVal lhr, IntVal rhr) -> BoolVal(lhr < rhr)
+        | _ -> failwith "integer value expected"
+    end
+    | And (lhr, rhr) -> begin match (eval2 lhr, eval2 rhr) with
+        | (BoolVal lhr, BoolVal rhr) -> BoolVal(lhr && rhr)
+        | _ -> failwith "integer value expected"
+    end
+    | Or (lhr, rhr) -> begin match (eval2 lhr, eval2 rhr) with
+        | (BoolVal lhr, BoolVal rhr) -> BoolVal(lhr || rhr)
+        | _ -> failwith "integer value expected"
+    end
+    | Not e -> begin match eval2 e with
+        | BoolVal b -> BoolVal (not b)
+        | _ -> failwith "boolean type expected"
+    end
+    | Eq(lhr, rhr) -> begin match (eval2 lhr, eval2 rhr) with
+        | (IntVal lhr, IntVal rhr) -> BoolVal (lhr = rhr)
+        | (BoolVal lhr, BoolVal rhr) -> BoolVal (lhr = rhr)
+        | _ -> failwith "integer type expected"
+    end
+    | If (cond, e1, e2) -> begin match eval2 cond with
+        | BoolVal true -> eval2 e1
+        | BoolVal false -> eval2 e2
+        | _ -> failwith "boolean type expected"
+    end
+
 let rec eval2b =
     let binop_ii f lhr rhr = match (eval2b lhr, eval2b rhr) with
         | (IntVal lhr, IntVal rhr) -> IntVal (f lhr rhr)
@@ -36,7 +88,11 @@ let rec eval2b =
     | Plus (lhr, rhr) -> binop_ii (+) lhr rhr
     | Sub (lhr, rhr) -> binop_ii (-) lhr rhr
     | Times (lhr, rhr) -> binop_ii ( * ) lhr rhr
-    | Div (lhr, rhr) -> binop_ii (/) lhr rhr
+    | Div (lhr, rhr) -> begin match (eval2 lhr, eval2 rhr) with
+        | (IntVal _, IntVal 0) -> failwith "divided by 0"
+        | (IntVal lhr, IntVal rhr) -> IntVal(lhr / rhr)
+        | _ -> failwith "integer value expected"
+    end
     | Greater (lhr, rhr) -> binop_ib (>) lhr rhr
     | Less (lhr, rhr) -> binop_ib (<) lhr rhr
     | And (lhr, rhr) -> binop_bb (&&) lhr rhr

ef4f0effcaef48c00a3bc59955610126578ba560
Nakano Masaki <namachan10777@gmail.com>
2020-10-16 17:11:46 +0900

    comment

--- code/lib/kadai2_5.ml
+++ code/lib/kadai2_5.ml
@@ -2,15 +2,16 @@ type exp_t =
     | IntLit of int
     | Plus of exp_t * exp_t
     | Times of exp_t * exp_t
-    | Sub of exp_t * exp_t
-    | Div of exp_t * exp_t
     | BoolLit of bool
     | If of exp_t * exp_t * exp_t
     | Eq of exp_t * exp_t
+    | Greater of exp_t * exp_t
+    (* 課題外 *)
     | And of exp_t * exp_t
     | Or of exp_t * exp_t
+    | Sub of exp_t * exp_t
+    | Div of exp_t * exp_t
     | Not of exp_t
-    | Greater of exp_t * exp_t
     | Less of exp_t * exp_t
 
 type value_t =
@@ -34,6 +35,7 @@ let rec eval2 =
         | _ -> failwith "integer value expected"
     end
     | Div (lhr, rhr) -> begin match (eval2 lhr, eval2 rhr) with
+        (* 分母が0の場合をパターンマッチで先に除外 *)
         | (IntVal _, IntVal 0) -> failwith "divided by 0"
         | (IntVal lhr, IntVal rhr) -> IntVal(lhr / rhr)
         | _ -> failwith "integer value expected"
@@ -70,14 +72,17 @@ let rec eval2 =
     end
 
 let rec eval2b =
+    (* Plus, Times, Sub向け *)
     let binop_ii f lhr rhr = match (eval2b lhr, eval2b rhr) with
         | (IntVal lhr, IntVal rhr) -> IntVal (f lhr rhr)
         | _ -> failwith "integer type expected"
     in
+    (* Less, Greater向け *)
     let binop_ib f lhr rhr = match (eval2b lhr, eval2b rhr) with
         | (IntVal lhr, IntVal rhr) -> BoolVal (f lhr rhr)
         | _ -> failwith "integer type expected"
     in
+    (* And, Or向け *)
     let binop_bb f lhr rhr = match (eval2b lhr, eval2b rhr) with
         | (BoolVal lhr, BoolVal rhr) -> BoolVal (f lhr rhr)
         | _ -> failwith "boolean type expected"
@@ -88,6 +93,7 @@ let rec eval2b =
     | Plus (lhr, rhr) -> binop_ii (+) lhr rhr
     | Sub (lhr, rhr) -> binop_ii (-) lhr rhr
     | Times (lhr, rhr) -> binop_ii ( * ) lhr rhr
+    (* Divは分母が0の場合の処理を入れる必要があるので使えない *)
     | Div (lhr, rhr) -> begin match (eval2 lhr, eval2 rhr) with
         | (IntVal _, IntVal 0) -> failwith "divided by 0"
         | (IntVal lhr, IntVal rhr) -> IntVal(lhr / rhr)
@@ -101,6 +107,7 @@ let rec eval2b =
         | BoolVal b -> BoolVal (not b)
         | _ -> failwith "boolean type expected"
     end
+    (* Eqはint、bool両方を受けるので使えない *)
     | Eq(lhr, rhr) -> begin match (eval2b lhr, eval2b rhr) with
         | (IntVal lhr, IntVal rhr) -> BoolVal (lhr = rhr)
         | (BoolVal lhr, BoolVal rhr) -> BoolVal (lhr = rhr)

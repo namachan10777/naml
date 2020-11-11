382d6f9f79c246bf5c9318d336769da304cdb696
Nakano Masaki <namachan10777@gmail.com>
2020-11-11 14:02:53 +0900

    env and report4

---
+++ code/lib/env.ml
@@ -0,0 +1,6 @@
+let emptyenv () = []
+let ext env x v = (x, v) :: env
+let rec lookup x env =
+    match env with
+    | [] -> failwith ("unbound variable: " ^ x)
+    | (y, v) :: tl ->  if x=y then v else lookup x tl

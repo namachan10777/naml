d421ff724d6fd9fbd70b7c80146c7f9253c2c2cd
Nakano Masaki <nakano.masaki.sj@alumni.tsukuba.ac.jp>
2021-01-22 07:34:42 +0900

    support let and var

--- code/lib/compile_to_cam.ml
+++ code/lib/compile_to_cam.ml
@@ -1,50 +1,56 @@
 open K7ast
 
+let rec position x venv =
+    match venv with
+    | [] -> failwith "no matching variable in environment"
+    | y :: venv -> if x = y then 0 else 1 + position x venv
+
 let compile ast =
-    let rec f = function
+    let rec f venv = function
     | IntLit (i) -> [Cam.Ldi i]
     | BoolLit (b) -> [Cam.Ldb b]
     | StrLit s -> [Cam.Lds s]
-    | Add (lhr, rhr)  -> binop Cam.Add  lhr rhr
-    | Mul (lhr, rhr)  -> binop Cam.Mul  lhr rhr
-    | Div (lhr, rhr)  -> binop Cam.Div  lhr rhr
-    | Mod (lhr, rhr)  -> binop Cam.Mod  lhr rhr
-    | Sub (lhr, rhr)  -> binop Cam.Sub  lhr rhr
-    | Gret (lhr, rhr) -> binop Cam.Gret lhr rhr
-    | Less (lhr, rhr) -> binop Cam.Less lhr rhr
-    | Eq (lhr, rhr)   -> binop Cam.Eq   lhr rhr
-    | Neq (lhr, rhr)  -> binop Cam.Neq  lhr rhr
-    | Not e -> (f e) @ [Cam.Not]
+    | Add (lhr, rhr)  -> binop venv Cam.Add  lhr rhr
+    | Mul (lhr, rhr)  -> binop venv Cam.Mul  lhr rhr
+    | Div (lhr, rhr)  -> binop venv Cam.Div  lhr rhr
+    | Mod (lhr, rhr)  -> binop venv Cam.Mod  lhr rhr
+    | Sub (lhr, rhr)  -> binop venv Cam.Sub  lhr rhr
+    | Gret (lhr, rhr) -> binop venv Cam.Gret lhr rhr
+    | Less (lhr, rhr) -> binop venv Cam.Less lhr rhr
+    | Eq (lhr, rhr)   -> binop venv Cam.Eq   lhr rhr
+    | Neq (lhr, rhr)  -> binop venv Cam.Neq  lhr rhr
+    | Not e -> (f venv e) @ [Cam.Not]
     | Or (lhr, rhr)   ->
-        (f lhr) @ [Cam.Test (
+        (f venv lhr) @ [Cam.Test (
             [Ldb true],
-            f rhr
+            f venv rhr
         )]
     | And (lhr, rhr)  ->
-        (f lhr) @ [Cam.Test (
-            f rhr,
+        (f venv lhr) @ [Cam.Test (
+            f venv rhr,
             [Ldb false]
         )]
     | If (cond, then_e, else_e) ->
-        (f cond) @ [
+        (f venv cond) @ [
             Test (
-                f then_e,
-                f else_e
+                f venv then_e,
+                f venv else_e
             )
         ]
-    | Seq (lhr, rhr) ->
-        (f lhr) @ (f rhr)
+    | Let (id, def, expr) ->
+        (f venv def) @ [Cam.Let] @ (f (id :: venv) expr) @ [Cam.EndLet]
+    | Var id ->
+        [Cam.Access (position id venv)]
+    | Fun _ -> failwith "fun is unsupported"
+    | Seq (lhr, rhr) -> (f venv lhr) @ (f venv rhr)
     | Match _ -> failwith "match is unsupported"
     | Emp -> failwith "emp is unsupported"
     | Cons _ -> failwith "cons is unsupported"
-    | Var _ -> failwith "var is unsupported"
-    | Let _ -> failwith "let is unsupported"
     | LetRec _ -> failwith "let rec is unsupported"
-    | Fun _ -> failwith "fun is unsupported"
     | App _ -> failwith "app is unsupported"
     | Tuple _ -> failwith "tuple is unsupported"
     | Builtin _ -> failwith "builtin is unsupported"
     | DebugPrint _ -> failwith "debugprint is unsupported"
-    and binop op lhr rhr =
-        (f rhr) @ (f lhr) @ [op]
+    and binop venv op lhr rhr =
+        (f venv rhr) @ (f venv lhr) @ [op]
     in f ast
--- code/test/compile_to_cam.ml
+++ code/test/compile_to_cam.ml
@@ -6,12 +6,12 @@ open OUnit2
 let test_add _ =
     let ast = parse_repl_string "1+2" in
     let insts = [Ldi 2; Ldi 1; Add] in
-    assert_equal (compile ast) (insts)
+    assert_equal (compile [] ast) (insts)
 
 let test_4arith _ =
     let ast = parse_repl_string "(1+2*3)/4" in
     let insts = [Ldi 4; Ldi 3; Ldi 2; Mul; Ldi 1; Add; Div] in
-    assert_equal (compile ast) (insts)
+    assert_equal (compile [] ast) (insts)
 
 let test_compares _ =
     let ast = parse_repl_string "true || 1 > 2 && 2 = 3" in
@@ -30,7 +30,7 @@ let test_compares _ =
             ]
         );
     ] in
-    assert_equal (compile ast) (insts)
+    assert_equal (compile [] ast) (insts)
 
 let test_if _ =
     let ast = parse_repl_string "if 1 = 1 then 1 else 2" in
@@ -43,7 +43,7 @@ let test_if _ =
             [Ldi 2]
         );
     ] in
-    assert_equal (compile ast) insts
+    assert_equal (compile [] ast) insts
 
 let test_seq _ =
     let ast = parse_repl_string "1; 2; 3" in
@@ -52,7 +52,17 @@ let test_seq _ =
         Ldi 2;
         Ldi 3;
     ] in
-    assert_equal (compile ast) insts
+    assert_equal (compile [] ast) insts
+
+let test_let _ =
+    let ast = parse_repl_string "let x = 1 in x" in
+    let insts = [
+        Ldi 1;
+        Let;
+        Access 0;
+        EndLet;
+    ] in
+    assert_equal (compile [] ast) insts
 
 let suite =
     "Compile_to_cam" >::: [
@@ -61,4 +71,5 @@ let suite =
         "test_compares" >:: test_compares;
         "test_if" >:: test_if;
         "test_seq" >:: test_seq;
+        "test_let" >:: test_let;
     ]

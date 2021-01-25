e23fe6d3902cf4c5ebea64861b83d2c09da11d6e
Nakano Masaki <nakano.masaki.sj@alumni.tsukuba.ac.jp>
2021-01-25 22:14:54 +0900

    impl Lex.match_space

--- compiler/lex.ml
+++ compiler/lex.ml
@@ -9,8 +9,24 @@ let count_newline s =
         else cnt acc (i_begin+1) i_end
     in cnt 0
 
+let match_space_char s i =
+    if i >= String.length s
+    then None
+    else if s.[i] = ' ' || s.[i] = '\t' || s.[i] = '\n' || s.[i] = '\r'
+    then Some (i+1)
+    else None
+
+let match_space = opt match_space_char
 let () =
     Test.assert_eq "count_newline 0 11" (count_newline "foo\nbar\nhoge" 0 11) 2;
     Test.assert_eq "count_newline 0 3" (count_newline "foo\nbar\nhoge" 0 3) 0;
     Test.assert_eq "count_newline 0 4" (count_newline "foo\nbar\nhoge" 0 4) 1;
     Test.assert_eq "count_newline 3 8" (count_newline "foo\nbar\nhoge" 3 8) 2;
+    Test.assert_eq "match_space_char \" abc\" 0" (match_space_char " abc" 0) (Some 1);
+    Test.assert_eq "match_space_char \" abc\" 1" (match_space_char " abc" 1) None;
+    Test.assert_eq "match_space_char \"abc\" 0" (match_space_char "abc" 0) None;
+    Test.assert_eq "match_space \" \\n\\r\\t\"" (match_space " \n\r\t" 0) (Some 4);
+    Test.assert_eq "match_space \" \\n\\r\\t\" from 2" (match_space " \n\r\t" 2) (Some 4);
+    Test.assert_eq "match_space \" \\n\\r\\thoge\" from 2 ends normal char" (match_space " \n\r\thoge" 2) (Some 4);
+    Test.assert_eq "match_space \" \\n\\r\\thoge\" ends normal char" (match_space " \n\r\thoge" 0) (Some 4);
+    Test.assert_eq "match_space \"hoo\"" (match_space "hoo" 0) None;

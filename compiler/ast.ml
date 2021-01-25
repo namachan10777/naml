017bc3986cc4e05f25c92f78069952d9ebb3c7c8
Nakano Masaki <nakano.masaki.sj@alumni.tsukuba.ac.jp>
2021-01-26 04:14:39 +0900

    parse Ast.Add and Ast.Sub

---
+++ compiler/ast.ml
@@ -0,0 +1,3 @@
+type t =
+    | Int of int
+    | Add of t * t
--- compiler/parser.ml
+++ compiler/parser.ml
@@ -41,3 +41,34 @@ type token_t =
 
 type input_t = token_t list
 [@@deriving show]
+
+type parsed_t = Ast.t * token_t list
+
+exception SyntaxError
+
+let rec split_with f = function
+    | hd :: tl ->
+        if f hd
+        then Some ([], tl)
+        else begin match split_with f tl with
+        | Some(first_half, second_half) -> Some(hd :: first_half, second_half)
+        | None -> None
+        end
+    | [] -> None
+
+let is_add_or_sub = function
+    | Add -> true
+    | Sub -> true
+    | _ -> false
+
+let rec parse input =
+    match (split_with is_add_or_sub @@ List.rev input, input) with
+    | (Some ([Int rhr], lhr), _) ->
+        Ast.Add (parse (List.rev lhr), Ast.Int rhr)
+    | (None, [Int i]) ->
+        Ast.Int i
+    | _ -> raise SyntaxError
+
+let () =
+    Test.assert_eq "parse_add" (parse [Int 0; Add; Int 1; Add; Int 2])
+    (Ast.Add (Ast.Add(Ast.Int 0, Ast.Int 1), Ast.Int 2));

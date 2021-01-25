3073d0c1e95606c78c51740558ebd8985d23daec
Nakano Masaki <nakano.masaki.sj@alumni.tsukuba.ac.jp>
2021-01-25 22:15:08 +0900

    impl Lex.match_alph

--- compiler/lex.ml
+++ compiler/lex.ml
@@ -16,7 +16,17 @@ let match_space_char s i =
     then Some (i+1)
     else None
 
+let match_alph_char s i =
+    if i >= String.length s
+    then None
+    else
+        let code = Char.code s.[i] in
+        if code >= 0x41 && code <= 0x5a || code >= 61 && code <= 0x7a
+        then Some(i+1)
+        else None
+
 let match_space = opt match_space_char
+let match_alph = opt match_alph_char
 let () =
     Test.assert_eq "count_newline 0 11" (count_newline "foo\nbar\nhoge" 0 11) 2;
     Test.assert_eq "count_newline 0 3" (count_newline "foo\nbar\nhoge" 0 3) 0;
@@ -30,3 +40,5 @@ let () =
     Test.assert_eq "match_space \" \\n\\r\\thoge\" from 2 ends normal char" (match_space " \n\r\thoge" 2) (Some 4);
     Test.assert_eq "match_space \" \\n\\r\\thoge\" ends normal char" (match_space " \n\r\thoge" 0) (Some 4);
     Test.assert_eq "match_space \"hoo\"" (match_space "hoo" 0) None;
+    Test.assert_eq "match_alph \"abc \"" (match_alph "abc " 0) (Some 3);
+    Test.assert_eq "match_alph \" abc\"" (match_alph " abc" 0) None;

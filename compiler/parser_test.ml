46707f61468416f72c1a624795422fd8c7dd9208
Nakano Masaki <nakano.masaki.sj@alumni.tsukuba.ac.jp>
2021-01-28 04:56:23 +0900

    module P = Parser in parser_test

--- compiler/parser_test.ml
+++ compiler/parser_test.ml
@@ -1,144 +1,146 @@
+module P = Parser
+
 let test name src right =
-    let left =Parser.parse @@ Lex.lex src @@ Lex.initial_pos "test.ml" in
+    let left =P.parse @@ Lex.lex src @@ Lex.initial_pos "test.ml" in
     try Test.assert_eq name left right with
     | Test.UnitTestError err ->
-        Printf.printf "left  : %s\nright : %s\n" (Parser.show left) (Parser.show right);
+        Printf.printf "left  : %s\nright : %s\n" (P.show left) (P.show right);
         raise @@ Test.UnitTestError err
 
 let () =
     test "parse_add" "0+1+2"
-    (Parser.Add (Parser.Add(Parser.Int 0, Parser.Int 1), Parser.Int 2));
+    (P.Add (P.Add(P.Int 0, P.Int 1), P.Int 2));
     test "parse_mul" "0*1*2"
-    (Parser.Mul (Parser.Mul(Parser.Int 0, Parser.Int 1), Parser.Int 2));
+    (P.Mul (P.Mul(P.Int 0, P.Int 1), P.Int 2));
     test "unary_minus" "1+-2"
-    (Parser.Add (Parser.Int 1, Parser.Neg (Parser.Int 2)));
+    (P.Add (P.Int 1, P.Neg (P.Int 2)));
     test "mod" "3 mod 2"
-    (Parser.Mod (Parser.Int 3, Parser.Int 2));
+    (P.Mod (P.Int 3, P.Int 2));
     test "add_r" "0+(1+2)"
-    (Parser.Add (Parser.Int 0, Parser.Paren (Parser.Add (Parser.Int 1, Parser.Int 2))));
+    (P.Add (P.Int 0, P.Paren (P.Add (P.Int 1, P.Int 2))));
     test "4arith" "3*(200+300)/4-5"
-    (Parser.Sub (
-        Parser.Div (
-            Parser.Mul (Parser.Int 3, Parser.Paren(Parser.Add (Parser.Int 200, Parser.Int 300))),
-            Parser.Int 4
+    (P.Sub (
+        P.Div (
+            P.Mul (P.Int 3, P.Paren(P.Add (P.Int 200, P.Int 300))),
+            P.Int 4
         ),
-        Parser.Int 5
+        P.Int 5
     ));
     test "bool" "3>2 && not not (1 < 0)"
-    (Parser.And (
-        Parser.Gret (Parser.Int 3, Parser.Int 2),
-        Parser.Not (Parser.Not (Parser.Paren (Parser.Less (Parser.Int 1, Parser.Int 0))))
+    (P.And (
+        P.Gret (P.Int 3, P.Int 2),
+        P.Not (P.Not (P.Paren (P.Less (P.Int 1, P.Int 0))))
     ));
     test "bool2"  "true && false || 1 = 2 && false"
-    (Parser.Or (
-        Parser.And (Parser.Bool true, Parser.Bool false),
-        Parser.And (Parser.Eq (Parser.Int 1, Parser.Int 2), Parser.Bool false)
+    (P.Or (
+        P.And (P.Bool true, P.Bool false),
+        P.And (P.Eq (P.Int 1, P.Int 2), P.Bool false)
     ));
     test "let simple" "let x = 1 in x"
-    (Parser.Let ("x", Parser.Int 1, Parser.Var "x"));
+    (P.Let ("x", P.Int 1, P.Var "x"));
     test "let rec" "let rec x = 1 in x"
-    (Parser.LetRec ("x", Parser.Int 1, Parser.Var "x"));
+    (P.LetRec ("x", P.Int 1, P.Var "x"));
     test "let add left" "1 + let x = 1 in x"
-    (Parser.Add (Parser.Int 1, Parser.Let ("x", Parser.Int 1, Parser.Var "x")));
+    (P.Add (P.Int 1, P.Let ("x", P.Int 1, P.Var "x")));
     test "let add right" "(let x = 1 in x) + 1"
-    (Parser.Add (Parser.Paren(Parser.Let ("x", Parser.Int 1, Parser.Var "x")), Parser.Int 1));
+    (P.Add (P.Paren(P.Let ("x", P.Int 1, P.Var "x")), P.Int 1));
     test "let complex" "let x = let y = 1 in y in let z = x in z"
-    (Parser.Let ("x", Parser.Let ("y", Parser.Int 1, Parser.Var "y"), Parser.Let ("z", Parser.Var "x", Parser.Var "z")));
+    (P.Let ("x", P.Let ("y", P.Int 1, P.Var "y"), P.Let ("z", P.Var "x", P.Var "z")));
     test "fun" "fun x y z -> x + y + z"
-    (Parser.Fun (["x"; "y"; "z"], Parser.Add (Parser.Add (Parser.Var "x", Parser.Var "y"), Parser.Var "z")));
+    (P.Fun (["x"; "y"; "z"], P.Add (P.Add (P.Var "x", P.Var "y"), P.Var "z")));
     test "fun" "(fun x y z -> x + y + z) 1"
-    (Parser.App (
-        Parser.Paren (Parser.Fun (["x"; "y"; "z"], Parser.Add (Parser.Add (Parser.Var "x", Parser.Var "y"), Parser.Var "z"))),
-    Parser.Int 1));
+    (P.App (
+        P.Paren (P.Fun (["x"; "y"; "z"], P.Add (P.Add (P.Var "x", P.Var "y"), P.Var "z"))),
+    P.Int 1));
     test "cons" "1 + 2 :: 3 :: [] = []"
-    (Parser.Eq (
-        Parser.Cons (Parser.Add (Parser.Int 1, Parser.Int 2), Parser.Cons (Parser.Int 3, Parser.Emp)),
-        Parser.Emp
+    (P.Eq (
+        P.Cons (P.Add (P.Int 1, P.Int 2), P.Cons (P.Int 3, P.Emp)),
+        P.Emp
     ));
     test "pipeline" "1 |> f |> g"
-    (Parser.Pipeline (Parser.Pipeline (Parser.Int 1, Parser.Var "f"), Parser.Var "g"));
+    (P.Pipeline (P.Pipeline (P.Int 1, P.Var "f"), P.Var "g"));
     test "@@" "f @@ g @@ 1"
-    (Parser.App (Parser.Var "f", Parser.App (Parser.Var "g", Parser.Int 1)));
+    (P.App (P.Var "f", P.App (P.Var "g", P.Int 1)));
     test "seq" "1; 2 |> f; 3"
-    (Parser.Seq (
-        Parser.Int 1,
-        Parser.Seq (
-            Parser.Pipeline (Parser.Int 2, Parser.Var "f"),
-            Parser.Int 3
+    (P.Seq (
+        P.Int 1,
+        P.Seq (
+            P.Pipeline (P.Int 2, P.Var "f"),
+            P.Int 3
         )
     ));
     test "match1" "match x with y -> 0 | z -> match z with a -> a"
-    (Parser.Match (Parser.Var "x", [
-        (Parser.PVar "y", Parser.Bool true, Parser.Int 0);
-        (Parser.PVar "z", Parser.Bool true, Parser.Match (Parser.Var "z", [
-            (Parser.PVar "a",  Parser.Bool true, Parser.Var "a");
+    (P.Match (P.Var "x", [
+        (P.PVar "y", P.Bool true, P.Int 0);
+        (P.PVar "z", P.Bool true, P.Match (P.Var "z", [
+            (P.PVar "a",  P.Bool true, P.Var "a");
         ]));
     ]));
     test "match_when" "match x with y when is_primve y -> 0 | z -> 1"
-    (Parser.Match (Parser.Var "x", [
-        (Parser.PVar "y", Parser.App (Parser.Var "is_primve", Parser.Var "y"), Parser.Int 0);
-        (Parser.PVar "z", Parser.Bool true, Parser.Int 1);
+    (P.Match (P.Var "x", [
+        (P.PVar "y", P.App (P.Var "is_primve", P.Var "y"), P.Int 0);
+        (P.PVar "z", P.Bool true, P.Int 1);
     ]));
     test "match2" "match x with y -> let x = 1 in x | z -> 1"
-    (Parser.Match (Parser.Var "x", [
-        (Parser.PVar "y", Parser.Bool true, Parser.Let ("x", Parser.Int 1, Parser.Var "x"));
-        (Parser.PVar "z", Parser.Bool true, Parser.Int 1);
+    (P.Match (P.Var "x", [
+        (P.PVar "y", P.Bool true, P.Let ("x", P.Int 1, P.Var "x"));
+        (P.PVar "z", P.Bool true, P.Int 1);
     ]));
     test "match nested" "match match [] with [] -> [] with [] -> []"
-    (Parser.Match (Parser.Match (Parser.Emp, [Parser.PEmp, Parser.Bool true, Parser.Emp]), [Parser.PEmp, Parser.Bool true, Parser.Emp]));
-    test "tuple1" "1, 2" (Parser.Tuple [Parser.Int 1; Parser.Int 2]);
+    (P.Match (P.Match (P.Emp, [P.PEmp, P.Bool true, P.Emp]), [P.PEmp, P.Bool true, P.Emp]));
+    test "tuple1" "1, 2" (P.Tuple [P.Int 1; P.Int 2]);
     test "tuple1" "1+2, 2+3"
-        (Parser.Tuple [
-            Parser.Add (Parser.Int 1, Parser.Int 2);
-            Parser.Add (Parser.Int 2, Parser.Int 3);
+        (P.Tuple [
+            P.Add (P.Int 1, P.Int 2);
+            P.Add (P.Int 2, P.Int 3);
         ]);
-    test "tuple1" "1, 2, 3, 4" (Parser.Tuple [Parser.Int 1; Parser.Int 2; Parser.Int 3; Parser.Int 4]);
-    test "tuple nest" "1, (3, 4)" (Parser.Tuple [Parser.Int 1; Parser.Paren (Parser.Tuple [Parser.Int 3; Parser.Int 4])]);
+    test "tuple1" "1, 2, 3, 4" (P.Tuple [P.Int 1; P.Int 2; P.Int 3; P.Int 4]);
+    test "tuple nest" "1, (3, 4)" (P.Tuple [P.Int 1; P.Paren (P.Tuple [P.Int 3; P.Int 4])]);
     test "pattern_tuple" "match x with (x, y) -> x"
-    (Parser.Match (Parser.Var "x", [
-        (Parser.PParen (Parser.PTuple [Parser.PVar "x"; Parser.PVar "y"]), Parser.Bool true, Parser.Var "x");
+    (P.Match (P.Var "x", [
+        (P.PParen (P.PTuple [P.PVar "x"; P.PVar "y"]), P.Bool true, P.Var "x");
     ]));
     test "pattern_tuple" "match x with (x, (y, z)) -> x"
-    (Parser.Match (Parser.Var "x", [
-        (Parser.PParen (Parser.PTuple [
-            Parser.PVar "x";
-            Parser.PParen (Parser.PTuple [Parser.PVar "y"; Parser.PVar "z"]);
-        ]), Parser.Bool true, Parser.Var "x");
+    (P.Match (P.Var "x", [
+        (P.PParen (P.PTuple [
+            P.PVar "x";
+            P.PParen (P.PTuple [P.PVar "y"; P.PVar "z"]);
+        ]), P.Bool true, P.Var "x");
     ]));
     test "pattern_cons" "match x with x :: [] -> x"
-    (Parser.Match (Parser.Var "x", [
-        (Parser.PCons (Parser.PVar "x", Parser.PEmp), Parser.Bool true, Parser.Var "x")
+    (P.Match (P.Var "x", [
+        (P.PCons (P.PVar "x", P.PEmp), P.Bool true, P.Var "x")
     ]));
-    test "parse_list1" "[1]" (Parser.Cons (Parser.Int 1, Parser.Emp));
-    test "parse_list2" "[1;]" (Parser.Cons (Parser.Int 1, Parser.Emp));
-    test "parse_list3" "[1;2;3]" (Parser.Cons (Parser.Int 1, Parser.Cons (Parser.Int 2, Parser.Cons (Parser.Int 3, Parser.Emp))));
-    test "parse_list4" "[1;2;3;]" (Parser.Cons (Parser.Int 1, Parser.Cons (Parser.Int 2, Parser.Cons (Parser.Int 3, Parser.Emp))));
-    test "parse_list5" "1 :: [2;3]" (Parser.Cons (Parser.Int 1, Parser.Cons (Parser.Int 2, Parser.Cons (Parser.Int 3, Parser.Emp))));
+    test "parse_list1" "[1]" (P.Cons (P.Int 1, P.Emp));
+    test "parse_list2" "[1;]" (P.Cons (P.Int 1, P.Emp));
+    test "parse_list3" "[1;2;3]" (P.Cons (P.Int 1, P.Cons (P.Int 2, P.Cons (P.Int 3, P.Emp))));
+    test "parse_list4" "[1;2;3;]" (P.Cons (P.Int 1, P.Cons (P.Int 2, P.Cons (P.Int 3, P.Emp))));
+    test "parse_list5" "1 :: [2;3]" (P.Cons (P.Int 1, P.Cons (P.Int 2, P.Cons (P.Int 3, P.Emp))));
     test "parse_list1" "match [] with [1] -> []"
-        (Parser.Match (Parser.Emp, [Parser.PCons (Parser.PInt 1, Parser.PEmp), Parser.Bool true, Parser.Emp]));
+        (P.Match (P.Emp, [P.PCons (P.PInt 1, P.PEmp), P.Bool true, P.Emp]));
     test "parse_pattern_list2" "match [] with [1;] -> []"
-        (Parser.Match (Parser.Emp, [Parser.PCons (Parser.PInt 1, Parser.PEmp), Parser.Bool true, Parser.Emp]));
+        (P.Match (P.Emp, [P.PCons (P.PInt 1, P.PEmp), P.Bool true, P.Emp]));
     test "parse_pattern_list3" "match [] with [1;2;3] -> []"
-        (Parser.Match (Parser.Emp, [Parser.PCons (Parser.PInt 1, Parser.PCons (Parser.PInt 2, Parser.PCons (Parser.PInt 3, Parser.PEmp))), Parser.Bool true, Parser.Emp]));
+        (P.Match (P.Emp, [P.PCons (P.PInt 1, P.PCons (P.PInt 2, P.PCons (P.PInt 3, P.PEmp))), P.Bool true, P.Emp]));
     test "parse_pattern_list4" "match [] with [1;2;3;] -> []"
-        (Parser.Match (Parser.Emp, [Parser.PCons (Parser.PInt 1, Parser.PCons (Parser.PInt 2, Parser.PCons (Parser.PInt 3, Parser.PEmp))), Parser.Bool true, Parser.Emp]));
+        (P.Match (P.Emp, [P.PCons (P.PInt 1, P.PCons (P.PInt 2, P.PCons (P.PInt 3, P.PEmp))), P.Bool true, P.Emp]));
     test "parse_pattern_list5" "match [] with 1 :: [2;3] -> []"
-        (Parser.Match (Parser.Emp, [Parser.PCons (Parser.PInt 1, Parser.PCons (Parser.PInt 2, Parser.PCons (Parser.PInt 3, Parser.PEmp))), Parser.Bool true, Parser.Emp]));
+        (P.Match (P.Emp, [P.PCons (P.PInt 1, P.PCons (P.PInt 2, P.PCons (P.PInt 3, P.PEmp))), P.Bool true, P.Emp]));
     test "app1" "1 + f 2"
-        (Parser.Add (Parser.Int 1, Parser.App (Parser.Var "f", Parser.Int 2)));
+        (P.Add (P.Int 1, P.App (P.Var "f", P.Int 2)));
     test "app2" "- f 2"
-        (Parser.Neg (Parser.App (Parser.Var "f", Parser.Int 2)));
+        (P.Neg (P.App (P.Var "f", P.Int 2)));
     test "app3" "f 1 + 2"
-        (Parser.Add (Parser.App (Parser.Var "f", Parser.Int 1), Parser.Int 2));
+        (P.Add (P.App (P.Var "f", P.Int 1), P.Int 2));
     test "app4" "f 1 2"
-        (Parser.App (Parser.App (Parser.Var "f", Parser.Int 1), Parser.Int 2));
+        (P.App (P.App (P.Var "f", P.Int 1), P.Int 2));
     test "app5" "f 1 2 3"
-        (Parser.App (Parser.App (Parser.App (Parser.Var "f", Parser.Int 1), Parser.Int 2), Parser.Int 3));
+        (P.App (P.App (P.App (P.Var "f", P.Int 1), P.Int 2), P.Int 3));
     test "if" "if f x then 1 else let x = 1 in x"
-        (Parser.If (Parser.App (Parser.Var "f", Parser.Var "x"), Parser.Int 1, Parser.Let ("x", Parser.Int 1, Parser.Var "x")));
+        (P.If (P.App (P.Var "f", P.Var "x"), P.Int 1, P.Let ("x", P.Int 1, P.Var "x")));
     test "if_nested" "if if x then true else false then true else if y then true else false"
-        (Parser.If (
-            Parser.If (Parser.Var "x", Parser.Bool true, Parser.Bool false),
-            Parser.Bool true,
-            Parser.If (Parser.Var "y", Parser.Bool true, Parser.Bool false)
+        (P.If (
+            P.If (P.Var "x", P.Bool true, P.Bool false),
+            P.Bool true,
+            P.If (P.Var "y", P.Bool true, P.Bool false)
         ));

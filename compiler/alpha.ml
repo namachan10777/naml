1bba991acafa00ca99a66e660a149c73c0d9bd2c
Nakano Masaki <nakano.masaki.sj@alumni.tsukuba.ac.jp>
2021-01-28 12:39:27 +0900

    alpha.ml

---
+++ compiler/alpha.ml
@@ -0,0 +1,48 @@
+type id_t = string list * int 
+[@@deriving show]
+
+type pat_t =
+    | PEmp
+    | PCons of pat_t * pat_t
+    | PInt of int
+    | PBool of bool
+    | PVar of string
+    | PTuple of pat_t list
+    | As of pat_t list
+    | PCtorApp of string list * pat_t
+    | PCtor of string list
+[@@deriving show]
+
+type t =
+    | Emp
+    | Int of int
+    | Bool of bool
+    | Var of string list
+    | Tuple of t list
+    | If of t * t * t
+    | Let of string * t * t
+    | LetRec of string * t * t
+    | Fun of string list * t
+    | Match of t * (pat_t * t * t) list
+    | App of t * t
+    | ArrayAssign of t * t * t
+[@@deriving show]
+
+type ty_t =
+    | TId of string list
+    | TVar of id_t
+    | TTuple of ty_t list
+    | TVariant of (id_t * ty_t) list
+    | TApp of ty_t * id_t
+[@@deriving show]
+
+type stmt_t =
+    | LetS of (pat_t * t) list
+    | LetRecS of (pat_t * t) list
+    | Type of (string * string list * ty_t) list
+[@@deriving show]
+
+let count = ref 0
+let fresh () =
+    count := 1 + !count;
+    string_of_int !count
--- compiler/typing.ml
+++ compiler/typing.ml
@@ -1,5 +1,8 @@
 (* Algorithm W*)
 
+type id_t = Alpha.id_t
+[@@deriving show]
+
 type ty_t =
     | Int
     | Str
@@ -7,6 +10,7 @@ type ty_t =
     | Arrow of ty_t * ty_t
     | List of ty_t
     | Var of string
+[@@deriving show]
 
 type pat_t =
     | PEmp of ty_t ref
@@ -16,20 +20,20 @@ type pat_t =
     | PVar of string * ty_t ref
     | PTuple of pat_t list * ty_t ref
     | As of pat_t list * ty_t ref
-    | PCtorApp of string list * pat_t * ty_t ref
-    | PCtor of string list * ty_t ref
+    | PCtorApp of id_t * pat_t * ty_t ref
+    | PCtor of id_t * ty_t ref
 [@@deriving show]
 
 type t =
     | Emp of ty_t ref
     | Int of int
     | Bool of bool
-    | Var of string list * ty_t ref
+    | Var of id_t * ty_t ref
     | Tuple of t list * ty_t ref
     | If of (t * ty_t ref) * (t * ty_t ref) * (t * ty_t ref)
     | Let of string * (t * ty_t ref) * (t * ty_t ref)
     | LetRec of string * (t * ty_t ref) * (t * ty_t ref)
-    | Fun of string list * (t * ty_t ref)
+    | Fun of id_t * (t * ty_t ref)
     | Match of (t * ty_t ref) * (pat_t * (t * ty_t ref) * (t * ty_t ref)) list
     | App of (t * ty_t ref) * (t * ty_t ref)
     | ArrayAssign of (t * ty_t ref) * (t * ty_t ref) * (t * ty_t ref)

82953186f3617f087923f0e7e983586437fbb3cb
Nakano Masaki <nakano.masaki.sj@alumni.tsukuba.ac.jp>
2021-01-29 04:48:27 +0900

    example

--- compiler/main.ml
+++ compiler/main.ml
@@ -1,1 +1,7 @@
-let () = print_endline "Hello World!"
+let () =
+    if (Array.length Sys.argv) != 2
+    then print_endline "compiler <filename>"
+    else
+        let ic = open_in Sys.argv.(1) in
+        let src = really_input_string ic (in_channel_length ic) in
+        print_endline @@ Parser.show_stmts_t @@ Parser.f Sys.argv.(1) src

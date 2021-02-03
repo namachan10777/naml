c07a73fa000488c799136cd37812a9eca41c5d16
Nakano Masaki <nakano.masaki.sj@alumni.tsukuba.ac.jp>
2021-02-04 04:26:16 +0900

    fix format script

--- code/format-check.sh
+++ code/format-check.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+cd $(dirname $0)
+
 find bin -type f -name '*.ml' | xargs ocamlformat --check $f && \
 find lib -type f -name '*.ml' | xargs ocamlformat --check $f && \
 find test -type f -name '*.ml' | xargs ocamlformat --check $f
--- compiler/format.sh
+++ compiler/format.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
 
+cd $(dirname $0)
+
 find . -type f -name '*.ml' | xargs ocamlformat -i $f

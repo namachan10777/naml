d5e2a77c8d45bc5fe612700667c78cef049d7996
Nakano Masaki <nakano.masaki.sj@alumni.tsukuba.ac.jp>
2021-02-10 16:21:13 +0900

    ack and tarai

---
+++ code/example/ack.ml
@@ -0,0 +1,7 @@
+let rec ack m n =
+    if m = 0
+    then n + 1
+    else if n = 0
+    then ack (m-1) 1
+    else ack (m-1) (ack m (n-1))
+    in ack 3 10
---
+++ figures/fib_ocaml.sh
@@ -0,0 +1,16 @@
+# time ocaml example/fib.ml
+14930352
+________________________________________________________
+Executed in  387.18 millis    fish           external
+   usr time  375.61 millis  518.00 micros  375.09 millis
+   sys time    6.71 millis  150.00 micros    6.56 millis
+
+
+# ocamlopt example/fib.ml
+
+# time ./a.out
+14930352
+________________________________________________________
+Executed in   64.86 millis    fish           external
+   usr time   64.79 millis  513.00 micros   64.28 millis
+   sys time    0.15 millis  150.00 micros    0.00 millis

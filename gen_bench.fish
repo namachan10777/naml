a07aae4c1c2943432be8892884d2af41e72dcc3b
Nakano Masaki <nakano.masaki.sj@alumni.tsukuba.ac.jp>
2021-02-24 07:44:45 +0900

    fix gen_bench.fish

--- gen_bench.fish
+++ gen_bench.fish
@@ -1,23 +1,23 @@
 #!/usr/bin/fish
 #
 echo "compiler2 asmgen ./compiler/example/$argv[1].ml -o a.out"
-compiler2 asmgen ./compiler/example/$argv[1].ml -o $argv[1].out
+./compiler2 asmgen ./compiler/example/$argv[1].ml -o $argv[1].out
 echo "time a.out"
-time $argv[1].out
+time ./$argv[1].out
 
 echo "ocamlopt ./compiler/example/$argv[1].ml -o a.out"
 ocamlopt ./compiler/example/$argv[1].ml -o $argv[1].out
 echo "time a.out"
-time $argv[1].out
+time ./$argv[1].out
 
 echo "time ocaml ./compiler/example/$argv[1].ml"
 time ocaml ./compiler/example/$argv[1].ml
 
 echo "time compiler1 tree ./code/example/$argv[1].ml"
-time compiler1 tree ./code/example/$argv[1].ml
+time ./compiler1 tree ./code/example/$argv[1].ml
 
 echo "time compiler1 cam ./code/example/$argv[1].ml"
-time compiler1 cam ./code/example/$argv[1].ml
+time ./compiler1 cam ./code/example/$argv[1].ml
 
 echo "time compiler1 zam ./code/example/$argv[1].ml"
-time compiler1 zam ./code/example/$argv[1].ml
+time ./compiler1 zam ./code/example/$argv[1].ml

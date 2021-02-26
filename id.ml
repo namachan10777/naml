ca0cd89a40f77a9a20bf7aaf49bf54ae91d621e5
Nakano Masaki <nakano.masaki.sj@alumni.tsukuba.ac.jp>
2021-02-26 14:57:38 +0900

    refactor id.ml

--- id.ml
+++ id.ml
@@ -1,1 +1,10 @@
-type t = string list * int
+type t = string list * string * int
+
+let show = function
+    | [], name, id -> Printf.sprintf "%s(%d)" name id
+    | prefix, name, id ->
+        Printf.sprintf "%s(%d)"
+          (List.fold_left
+             (fun name ident -> ident ^ "." ^ name)
+             name (List.rev prefix))
+          id

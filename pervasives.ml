a6a9835f005575afb53e58844e15d86277c1eefd
Nakano Masaki <nakano.masaki.sj@alumni.tsukuba.ac.jp>
2021-02-26 23:46:25 +0900

    start to define pervasive types

---
+++ pervasives.ml
@@ -0,0 +1,4 @@
+let vars = [
+    Id.from_strlist ["="], (1, Types.Fun (Types.Poly 0, Types.Fun (Types.Poly 0, Types.Bool)));
+    Id.from_strlist ["<>"], (1, Types.Fun (Types.Poly 0, Types.Fun (Types.Poly 0, Types.Bool)));
+]

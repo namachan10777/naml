c71b325e176aedf8fb47bc1aed51aeb5cdad3127
Nakano Masaki <nakano.masaki.sj@alumni.tsukuba.ac.jp>
2021-02-26 15:02:51 +0900

    refactor lex_test

--- Makefile
+++ Makefile
@@ -15,58 +15,14 @@ clean:
 	rm -f *.pp.ml
 
 .PHONY: test
-test: lex_test parser_test typing_test
+test: lex_test
 	./lex_test
-	./parser_test
-	./typing_test
 
-test.cmx: test.ml
+lex.cmx: lex.ml
 	$(OCAMLOPT) $< -c
 
-ast.cmx: ast.ml parser.cmx
+lex_test.cmx: lex_test.ml lex.cmx
 	$(OCAMLOPT) $< -c
 
-lex.cmx: lex.ml test.cmx
-	$(OCAMLOPT) $< -c
-
-lex_test.cmx: lex_test.ml parser.cmx test.cmx
-	$(OCAMLOPT) $< -c
-
-lex_test: test.cmx lex.cmx lex_test.cmx 
+lex_test: lex.cmx lex_test.cmx
 	$(OCAMLOPT) $^ -o $@
-
-parser_test.cmx: parser_test.ml parser.cmx lex.cmx util.cmx
-	$(OCAMLOPT) $< -c
-
-id.cmx: id.ml
-	$(OCAMLOPT) $< -c
-
-parser.cmx: parser.ml lex.cmx
-	$(OCAMLOPT) $< -c
-
-parser_test: lex.cmx test.cmx parser.cmx util.cmx parser_test.cmx 
-	$(OCAMLOPT) $^ -o $@
-
-alpha.cmx: alpha.ml types.cmx ast.cmx
-	$(OCAMLOPT) $< -c
-
-typing.cmx: typing.ml util.cmx types.cmx alpha.cmx
-	$(OCAMLOPT) $< -c
-
-typing_test.cmx: typing_test.ml util.cmx typing.cmx util.cmx ast.cmx alpha.cmx
-	$(OCAMLOPT) $< -c
-
-typing_test: lex.cmx parser.cmx ast.cmx util.cmx id.cmx types.cmx alpha.cmx typing.cmx test.cmx  util.cmx alpha.cmx typing_test.cmx
-	$(OCAMLOPT) $^ -o $@
-
-util.cmx: util.ml
-	$(OCAMLOPT) $< -c
-
-types.cmx: types.ml util.cmx id.cmx
-	$(OCAMLOPT) $< -c
-
-main.cmx: main.ml parser.cmx lex.cmx types.cmx
-	$(OCAMLOPT) $< -c
-
-1st: util.cmx lex.cmx parser.cmx ast.cmx id.cmx types.cmx alpha.cmx typing.cmx main.cmx
-	ocamlopt $^ -o $@
--- lex_test.ml
+++ lex_test.ml
@@ -2,118 +2,123 @@ open Lex
 
 type t = (Lex.t * Lex.pos_t) list [@@deriving show]
 
+let assert_eq name a b =
+    if a = b
+    then ()
+    else failwith @@ Printf.sprintf "test failed %s" name
+
 let () =
-    Test.assert_eq "count_newline 0 11" (count_newline "foo\nbar\nhoge" 0 11) 2 ;
-    Test.assert_eq "count_newline 0 3" (count_newline "foo\nbar\nhoge" 0 3) 0 ;
-    Test.assert_eq "count_newline 0 4" (count_newline "foo\nbar\nhoge" 0 4) 1 ;
-    Test.assert_eq "count_newline 3 8" (count_newline "foo\nbar\nhoge" 3 8) 2 ;
-    Test.assert_eq "match_space_char \" abc\" 0"
+    assert_eq "count_newline 0 11" (count_newline "foo\nbar\nhoge" 0 11) 2 ;
+    assert_eq "count_newline 0 3" (count_newline "foo\nbar\nhoge" 0 3) 0 ;
+    assert_eq "count_newline 0 4" (count_newline "foo\nbar\nhoge" 0 4) 1 ;
+    assert_eq "count_newline 3 8" (count_newline "foo\nbar\nhoge" 3 8) 2 ;
+    assert_eq "match_space_char \" abc\" 0"
       (match_space_char " abc" 0)
       (Some 1) ;
-    Test.assert_eq "match_space_char \" abc\" 1"
+    assert_eq "match_space_char \" abc\" 1"
       (match_space_char " abc" 1)
       None ;
-    Test.assert_eq "match_space_char \"abc\" 0" (match_space_char "abc" 0) None ;
-    Test.assert_eq "match_space \" \\n\\r\\t\"" (match_space " \n\r\t" 0)
+    assert_eq "match_space_char \"abc\" 0" (match_space_char "abc" 0) None ;
+    assert_eq "match_space \" \\n\\r\\t\"" (match_space " \n\r\t" 0)
       (Some 4) ;
-    Test.assert_eq "match_space \" \\n\\r\\t\" from 2" (match_space " \n\r\t" 2)
+    assert_eq "match_space \" \\n\\r\\t\" from 2" (match_space " \n\r\t" 2)
       (Some 4) ;
-    Test.assert_eq "match_space \" \\n\\r\\thoge\" from 2 ends normal char"
+    assert_eq "match_space \" \\n\\r\\thoge\" from 2 ends normal char"
       (match_space " \n\r\thoge" 2)
       (Some 4) ;
-    Test.assert_eq "match_space \" \\n\\r\\thoge\" ends normal char"
+    assert_eq "match_space \" \\n\\r\\thoge\" ends normal char"
       (match_space " \n\r\thoge" 0)
       (Some 4) ;
-    Test.assert_eq "match_space \"hoo\"" (match_space "hoo" 0) None ;
-    Test.assert_eq "match_lower_char \"a \""
+    assert_eq "match_space \"hoo\"" (match_space "hoo" 0) None ;
+    assert_eq "match_lower_char \"a \""
       (match_alph_lower_char "a " 0)
       (Some 1) ;
-    Test.assert_eq "match_upper_char \"A \""
+    assert_eq "match_upper_char \"A \""
       (match_alph_upper_char "A " 0)
       (Some 1) ;
-    Test.assert_eq "match_lower_char \"A \"" (match_alph_lower_char "A " 0) None ;
-    Test.assert_eq "match_upper_char \"a \"" (match_alph_upper_char "a " 0) None ;
-    Test.assert_eq "match_lower_ident \" abc\""
+    assert_eq "match_lower_char \"A \"" (match_alph_lower_char "A " 0) None ;
+    assert_eq "match_upper_char \"a \"" (match_alph_upper_char "a " 0) None ;
+    assert_eq "match_lower_ident \" abc\""
       (match_lower_ident " abc" 0)
       None ;
-    Test.assert_eq "match_lower_ident \"abc \""
+    assert_eq "match_lower_ident \"abc \""
       (match_lower_ident "abc " 0)
       (Some 3) ;
-    Test.assert_eq "match_lower_ident \" abc\""
+    assert_eq "match_lower_ident \" abc\""
       (match_lower_ident " abc" 0)
       None ;
-    Test.assert_eq "match_int \"123a\"" (match_int "123a" 0) (Some 3) ;
-    Test.assert_eq "match_int \"a123\"" (match_int "a123" 0) None ;
-    Test.assert_eq "match_hexint \"0x123g\"" (match_hexint "0x123g" 0) (Some 5) ;
-    Test.assert_eq "match_hexint \"a0x123\"" (match_hexint "a0x123" 0) None ;
-    Test.assert_eq "match_char '.' \".a\"" (match_char '.' ".a" 0) (Some 1) ;
-    Test.assert_eq "match_char '.' \"a.\"" (match_char '.' "a." 0) None ;
-    Test.assert_eq "match_str \"hoge\" \"hoge\""
+    assert_eq "match_int \"123a\"" (match_int "123a" 0) (Some 3) ;
+    assert_eq "match_int \"a123\"" (match_int "a123" 0) None ;
+    assert_eq "match_hexint \"0x123g\"" (match_hexint "0x123g" 0) (Some 5) ;
+    assert_eq "match_hexint \"a0x123\"" (match_hexint "a0x123" 0) None ;
+    assert_eq "match_char '.' \".a\"" (match_char '.' ".a" 0) (Some 1) ;
+    assert_eq "match_char '.' \"a.\"" (match_char '.' "a." 0) None ;
+    assert_eq "match_str \"hoge\" \"hoge\""
       (match_str "hoge" "hoge" 0)
       (Some 4) ;
-    Test.assert_eq "match_str \"hoge\" \"hog\"" (match_str "hoge" "hog" 0) None ;
-    Test.assert_eq "match_str \"hoge\" \"hogu\""
+    assert_eq "match_str \"hoge\" \"hog\"" (match_str "hoge" "hog" 0) None ;
+    assert_eq "match_str \"hoge\" \"hogu\""
       (match_str "hoge" "hogu" 0)
       None ;
-    Test.assert_eq "match_str \"hoge\" \" hoge\""
+    assert_eq "match_str \"hoge\" \" hoge\""
       (match_str "hoge" " hoge" 1)
       (Some 5) ;
-    Test.assert_eq "match_strlit \"\"hoge\""
+    assert_eq "match_strlit \"\"hoge\""
       (match_strlit "\"hoge\"" 0)
       (Some 6) ;
-    Test.assert_eq "match_strlit \"\"ho\\\\ge\""
+    assert_eq "match_strlit \"\"ho\\\\ge\""
       (match_strlit "\"ho\\\\ge\"" 0)
       (Some 8) ;
-    Test.assert_eq "match_strlit \"\"ho\\\"ge\""
+    assert_eq "match_strlit \"\"ho\\\"ge\""
       (match_strlit "\"ho\\\"ge\"" 0)
       (Some 8) ;
-    Test.assert_eq "match_strlit \"\"hoge\"" (match_strlit "\"hoge" 0) None ;
-    Test.assert_eq "match_strlit \"hoge\"\"" (match_strlit "hoge\"" 0) None ;
-    Test.assert_eq "match_strlit \"\"\"\"" (match_strlit "\"\"" 0) (Some 2) ;
-    Test.assert_eq "match_charlit 'a'" (match_charlit "'a'" 0) (Some 3) ;
-    Test.assert_eq "match_charlit '\\\''" (match_charlit "'\\\''" 0) (Some 4) ;
-    Test.assert_eq "update 1"
+    assert_eq "match_strlit \"\"hoge\"" (match_strlit "\"hoge" 0) None ;
+    assert_eq "match_strlit \"hoge\"\"" (match_strlit "hoge\"" 0) None ;
+    assert_eq "match_strlit \"\"\"\"" (match_strlit "\"\"" 0) (Some 2) ;
+    assert_eq "match_charlit 'a'" (match_charlit "'a'" 0) (Some 3) ;
+    assert_eq "match_charlit '\\\''" (match_charlit "'\\\''" 0) (Some 4) ;
+    assert_eq "update 1"
       (update_pos "hoge" (initial_pos "test.ml") 4)
       ("test.ml", 1, 4, 4) ;
-    Test.assert_eq "update 2"
+    assert_eq "update 2"
       (update_pos "ho\nge" (initial_pos "test.ml") 5)
       ("test.ml", 2, 2, 5) ;
-    Test.assert_eq "update 1"
+    assert_eq "update 1"
       (update_pos "hoge\n" (initial_pos "test.ml") 5)
       ("test.ml", 2, 0, 5) ;
-    Test.assert_eq "update 1"
+    assert_eq "update 1"
       (update_pos "\nhoge" (initial_pos "test.ml") 5)
       ("test.ml", 2, 4, 5) ;
-    Test.assert_eq "f g 0"
+    assert_eq "f g 0"
       (lex "f g 0" (initial_pos ""))
       [ (Lex.LIdent "f", ("", 1, 1, 1))
       ; (Lex.LIdent "g", ("", 1, 3, 3))
       ; (Lex.Int 0, ("", 1, 5, 5))
       ; (Lex.Eof, ("", 1, 5, 5)) ] ;
-    Test.assert_eq "lex strlit and space"
+    assert_eq "lex strlit and space"
       (lex " \"hoge\" \"foo\"" (initial_pos ""))
       [ (Lex.Str "hoge", ("", 1, 2, 2))
       ; (Lex.Str "foo", ("", 1, 9, 9))
       ; (Lex.Eof, ("", 1, 13, 13)) ] ;
-    Test.assert_eq "lex int and space"
+    assert_eq "lex int and space"
       (lex "123 0xff" (initial_pos ""))
       [ (Lex.Int 123, ("", 1, 1, 1))
       ; (Lex.Int 255, ("", 1, 5, 5))
       ; (Lex.Eof, ("", 1, 8, 8)) ] ;
-    Test.assert_eq "lex ident and space"
+    assert_eq "lex ident and space"
       (lex "aaa bbb" (initial_pos ""))
       [ (Lex.LIdent "aaa", ("", 1, 1, 1))
       ; (Lex.LIdent "bbb", ("", 1, 5, 5))
       ; (Lex.Eof, ("", 1, 7, 7)) ] ;
-    Test.assert_eq "'a' '\\''"
+    assert_eq "'a' '\\''"
       (lex "'a' '\\''" (initial_pos ""))
       [ (Lex.Char 'a', ("", 1, 1, 1))
       ; (Lex.Char '\'', ("", 1, 5, 5))
       ; (Lex.Eof, ("", 1, 8, 8)) ] ;
-    Test.assert_eq "tvar 'a "
+    assert_eq "tvar 'a "
       (lex "'a" (initial_pos ""))
       [(Lex.TVar "a", ("", 1, 1, 1)); (Lex.Eof, ("", 1, 2, 2))] ;
-    Test.assert_eq "lex [x]"
+    assert_eq "lex [x]"
       (lex "[x]" (initial_pos ""))
       [ (Lex.LB, ("", 1, 1, 1))
       ; (Lex.LIdent "x", ("", 1, 2, 2))
@@ -126,7 +131,7 @@ let () =
           (initial_pos "test.ml")
         |> ignore
       with LexException p -> Printf.printf "%s\n" @@ Lex.show_pos_t p ) ;
-    Test.assert_eq "lex fib"
+    assert_eq "lex fib"
       (lex
          "let rec fib n = if n = 1 || n = 0 then 1 else (fib (n-1)) + fib (n-2) \n\
           in fib 5" (initial_pos ""))

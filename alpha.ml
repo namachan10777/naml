bbbfbd899e9805ce45a84f51a6fa83ef331e2a6e
Nakano Masaki <nakano.masaki.sj@alumni.tsukuba.ac.jp>
2021-02-27 09:48:20 +0900

    format

--- alpha.ml
+++ alpha.ml
@@ -1,15 +1,17 @@
 type pat_tbl = (string list, Id.t) Tbl.t * (string list, unit) Tbl.t ref
 
 let f_pat (or_tbl, def_tbl) = function
-    | Ast.PVar (id, p) ->
+    | Ast.PVar (id, p) -> (
         let name = Id.name id in
-        begin match Tbl.lookup name or_tbl, Tbl.lookup name !def_tbl with
-        | Some id, _ -> [id], (Ast.PVar (id, p))
+        match (Tbl.lookup name or_tbl, Tbl.lookup name !def_tbl) with
+        | Some id, _ -> ([id], Ast.PVar (id, p))
         | None, None ->
-            def_tbl := Tbl.push (Id.name id) () !def_tbl;
-            [id], (Ast.PVar (id, p))
-        | None, Some _ -> failwith @@ Printf.sprintf "%s duplicated id %s" (Lex.show_pos_t p) (Id.show id)
-    end
+            def_tbl := Tbl.push (Id.name id) () !def_tbl ;
+            ([id], Ast.PVar (id, p))
+        | None, Some _ ->
+            failwith
+            @@ Printf.sprintf "%s duplicated id %s" (Lex.show_pos_t p)
+                 (Id.show id) )
     | p -> ([], p)
 
 let register_names tbl =
--- alpha_test.ml
+++ alpha_test.ml
@@ -1,5 +1,11 @@
 let f s = s |> Ast.f "test.ml" |> Alpha.f Tbl.empty
 
-let () = match f "let x = 1 and y = 2" with
-    | Ast.Let ([Ast.PVar (id1, _), _, Ast.Int (1, _); Ast.PVar (id2, _), _, Ast.Int (2, _)], Ast.Never, _) -> ()
+let () =
+    match f "let x = 1 and y = 2" with
+    | Ast.Let
+        ( [ (Ast.PVar (id1, _), _, Ast.Int (1, _))
+          ; (Ast.PVar (id2, _), _, Ast.Int (2, _)) ]
+        , Ast.Never
+        , _ ) ->
+        ()
     | _ -> failwith "ast test 1 failed"

9353b1cd908fcc13a837526f18724d72a934ba32
Nakano Masaki <nakano.masaki.sj@alumni.tsukuba.ac.jp>
2021-03-01 01:03:51 +0900

    test cannot unify

--- typing_test.ml
+++ typing_test.ml
@@ -50,8 +50,27 @@ let unify_fun () =
     assert_eq "unify fun" (!Typing.store).(0) (Typing.Just (Typing.TInt, [0]));
     assert_eq "unify fun" (!Typing.store).(1) (Typing.Just (Typing.TInt, [1]))
 
+let cannot_unify () =
+    Typing.init ();
+    let u1 = Typing.fresh 0 in
+    let u2 = Typing.fresh 0 in
+    (try Typing.unify Typing.TInt Typing.TStr; failwith "unexpected unify success" with
+    | Typing.UnifyError -> ());
+    (try Typing.unify (Typing.TFun (Typing.TStr, u1)) (Typing.TFun (Typing.TInt, u2)); failwith "unexpected unify success" with
+    | Typing.UnifyError -> ());
+    Typing.init ();
+    let u1 = Typing.fresh 0 in
+    let u2 = Typing.fresh 0 in
+    Typing.unify u1 Typing.TInt;
+    (try Typing.unify u1 Typing.TStr; failwith "unexpected unify success" with
+    | Typing.UnifyError -> ());
+    Typing.unify u2 Typing.TStr;
+    (try Typing.unify u1 u2; failwith "unexpected unify success" with
+    | Typing.UnifyError -> ())
+
 let () =
     unify_unk_unk ();
     unify_unk_ty ();
     unify_unk_just ();
-    unify_fun ()
+    unify_fun ();
+    cannot_unify ()

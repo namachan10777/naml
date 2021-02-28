f33046dd1afe5ccab13da5fddcbc425d870f2fa2
Nakano Masaki <nakano.masaki.sj@alumni.tsukuba.ac.jp>
2021-02-28 23:31:50 +0900

    Typing.fresh

--- typing.ml
+++ typing.ml
@@ -7,8 +7,9 @@ type ty =
     | Poly of int
     | Var of ty_var_t
     | Variant of ty list * Id.t
+[@@deriving show]
 
-and ty_var_t = Just of ty * int list | Unknown of int list
+and ty_var_t = Just of ty * int list | Unknown of int list [@@deriving show]
 
 type pat_t =
     | PInt of int * Lex.pos_t
@@ -38,3 +39,21 @@ type t =
     | Match of (t * ty) * ((pat_t * ty) * Lex.pos_t * t * (t * ty)) list
     | App of (t * ty) * (t * ty) * Lex.pos_t
     | Type of (Id.t * Lex.pos_t * (string * Lex.pos_t) list * tydef_t) list * t
+
+let store = ref @@ Array.init 2 (fun i -> Unknown [i])
+
+let count = ref 0
+
+let fresh () =
+    let arr_len = Array.length !store in
+    if !count >= arr_len then (
+      store :=
+        Array.concat
+          [!store; Array.init arr_len (fun i -> Unknown [i + arr_len])] ;
+      let ret = !store.(!count) in
+      count := 1 + !count ;
+      ret )
+    else
+      let ret = !store.(!count) in
+      count := 1 + !count ;
+      ret

be7104188b20efbd420d5349a22847f01ff276f0
Nakano Masaki <nakano.masaki.sj@alumni.tsukuba.ac.jp>
2021-03-01 12:27:28 +0900

    fix Typing.fresh

--- typing.ml
+++ typing.ml
@@ -9,7 +9,7 @@ type ty =
     | TVariant of ty list * Id.t
 [@@deriving show]
 
-and ty_var_t = Just of ty * int list | Unknown of int * int list [@@deriving show]
+and ty_var_t = Just of ty * int list | Unknown of int * int * int list [@@deriving show]
 
 exception UnifyError
 
@@ -42,13 +42,13 @@ type t =
     | App of (t * ty) * (t * ty) * Lex.pos_t
     | Type of (Id.t * Lex.pos_t * (string * Lex.pos_t) list * tydef_t) list * t
 
-let store = ref @@ Array.init 2 (fun i -> Unknown (0, [i]))
+let store = ref @@ Array.init 2 (fun i -> Unknown (0, i, [i]))
 type tenv_t = ty_var_t array [@@deriving show]
 
 let count = ref 0
 
 let init () =
-    store := Array.init 2 (fun i -> Unknown (0, [i]));
+    store := Array.init 2 (fun i -> Unknown (0, i, [i]));
     count := 0
 
 let fresh level =
@@ -56,22 +56,29 @@ let fresh level =
     if !count >= arr_len then (
       store :=
         Array.concat
-          [!store; Array.init arr_len (fun i -> Unknown (level, [i + arr_len]))] ;
+          [!store; Array.init arr_len (fun i -> Unknown (0, i+arr_len, [i + arr_len]))] ;
+      let idx = !count in
       count := 1 + !count ;
-      TyVar (!count - 1) )
+      !store.(idx) <- Unknown(level, idx, [idx]);
+      TyVar idx )
     else (
+      let idx = !count in
       count := 1 + !count ;
-      TyVar (!count - 1))
+      !store.(idx) <- Unknown(level, idx, [idx]);
+      TyVar idx)
 
 let rec unify t1 t2 = match t1, t2 with
     | TyVar v1, TyVar v2 -> begin match !store.(v1), !store.(v2) with
-        | Unknown (level1, l1), Unknown (level2, l2) ->
+        | Unknown (level1, tag, l1), Unknown (level2, _, l2) when level1 < level2 ->
             let l = l1 @ l2 in
-            List.map (fun i -> !store.(i) <- Unknown (min level1 level2, l)) l |> ignore
-        | Unknown (_, l1), Just (ty, l2) ->
+            List.map (fun i -> !store.(i) <- Unknown (level1, tag, l)) l |> ignore
+        | Unknown (level1, _, l1), Unknown (level2, tag, l2) ->
+            let l = l1 @ l2 in
+            List.map (fun i -> !store.(i) <- Unknown (level2, tag, l)) l |> ignore
+        | Unknown (_, _, l1), Just (ty, l2) ->
             let l = l1 @ l2 in
             List.map (fun i -> !store.(i) <- Just (ty, l)) l |> ignore
-        | Just (ty, l1), Unknown (_, l2) ->
+        | Just (ty, l1), Unknown (_, _, l2) ->
             let l = l1 @ l2 in
             List.map (fun i -> !store.(i) <- Just (ty, l)) l |> ignore
         | Just (ty1, l1), Just (ty2, l2) ->
@@ -80,12 +87,12 @@ let rec unify t1 t2 = match t1, t2 with
             List.map (fun i -> !store.(i) <- Just (ty1, l)) l |> ignore
     end
     | TyVar v, ty -> begin match !store.(v) with
-        | Unknown (_, l) ->
+        | Unknown (_, _, l) ->
             List.map (fun i -> !store.(i) <- Just (ty, l)) l |> ignore
         | Just (ty', l) -> unify ty ty'
     end
     | ty, TyVar v -> begin match !store.(v) with
-        | Unknown (_, l) ->
+        | Unknown (_, _, l) ->
             List.map (fun i -> !store.(i) <- Just (ty, l)) l |> ignore
         | Just (ty', l) -> unify ty ty'
     end
--- typing_test.ml
+++ typing_test.ml
@@ -8,14 +8,14 @@ let unify_unk_unk () =
     let u1 = Typing.fresh 0 in
     let u2 = Typing.fresh 1 in
     Typing.unify u1 u2;
-    assert_eq "unify to low level" (!Typing.store).(0) (Typing.Unknown (0, [0;1]));
-    assert_eq "unify to low level" (!Typing.store).(1) (Typing.Unknown (0, [0;1]));
+    assert_eq "unify to low level" (!Typing.store).(0) (Typing.Unknown (0, 0, [0;1]));
+    assert_eq "unify to low level" (!Typing.store).(1) (Typing.Unknown (0, 0, [0;1]));
     Typing.init ();
     let u1 = Typing.fresh 1 in
     let u2 = Typing.fresh 0 in
     Typing.unify u1 u2;
-    assert_eq "unify to low level" (!Typing.store).(0) (Typing.Unknown (0, [0;1]));
-    assert_eq "unify to low level" (!Typing.store).(1) (Typing.Unknown (0, [0;1]))
+    assert_eq "unify to low level" (!Typing.store).(0) (Typing.Unknown (0, 1, [0;1]));
+    assert_eq "unify to low level" (!Typing.store).(1) (Typing.Unknown (0, 1, [0;1]))
 
 let unify_unk_ty () =
     Typing.init ();

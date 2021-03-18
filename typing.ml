a71e9b4094492f478a5d7ab205cd855bc84eac00
Nakano Masaki <nakano.masaki.sj@alumni.tsukuba.ac.jp>
2021-03-18 20:47:52 +0900

    remove TODO

--- typing.ml
+++ typing.ml
@@ -434,7 +434,6 @@ let canonicalize_type_defs tenv (defs: canonicalize_type_defs_input_t) =
     let tydefs, ctors = Util.unzip @@ List.map canonicalize_type_def defs in
     tydefs, List.concat ctors
 
-(* TODO 値制限の導入 *)
 let rec f level env =
     let venv, cenv, tenv = env in
     function

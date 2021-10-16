69e05fadfdb2a08e3e229131b6d6d974fbffb922
Masaki Nakano <admin@namachan10777.dev>
2021-10-16 20:55:35 +0900

    main dir

--- old/main.ml
+++
@@ -1,8 +0,0 @@
-let () =
-  let ic = open_in Sys.argv.(1) in
-  let src = really_input_string ic (in_channel_length ic) in
-  let env, lets = Flatlet.typing_module Flatlet.pervasives Sys.argv.(1) src in
-  let funs, ast = Flat.f @@ Closure.f Closure.pervasives lets in
-  close_in ic ;
-  print_endline @@ Flat.show_funs_t funs;
-  print_endline @@ Flat.show_stmts_t ast
